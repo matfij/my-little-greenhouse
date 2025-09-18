647c2e91a77f5cd3838d507fe1db6bd5461aeb08
matfij <mateusz.michal.fijak@gmail.com>
2025-09-18 20:59:02 +0200

    Setup web client

---
+++ Source/Client/src/app-component.tsx
@@ -0,0 +1,3 @@
+export const AppComponent = () => {
+    return <h1>My Little Greenhouse Client</h1>;
+};
---
+++ Source/Client/src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AppComponent } from './app-component';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+    <StrictMode>
+        <AppComponent />
+    </StrictMode>,
+);
